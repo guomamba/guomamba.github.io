550923cb20141a3187358a37746e640859dc8ddf
guomamba <1185862603@qq.com>
2022-10-21 16:30:57 +0800

    Site updated: 2022-10-21 16:30:55

---
+++ pages/FlipClock/timer.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded',function(){
+    var item = document.getElementById("background")
+    item.style.height = window.screen.availHeight + "px"
+})
+
+var showtime = function (type) {
+    var nowdate = new Date();
+    switch (type) {
+        case "hour":
+            return checkTime(nowdate.getHours());
+        case "minute":
+            return checkTime(nowdate.getMinutes());
+        case "second":
+            return checkTime(nowdate.getSeconds());
+        default:
+            break;
+    }
+}
+
+var checkTime = function (i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+var hour = document.getElementById("showhour");
+hour.style.fontSize = 1.5*window.screen.availHeight/2 + "px"
+var minute = document.getElementById("showminute");
+minute.style.fontSize = 1.5*window.screen.availHeight/2 + "px"
+var second = document.getElementById("showsecond");
+setInterval (function() {
+    var h = "hour"
+    hour.textContent = showtime(h);
+    var m = "minute"
+    minute.textContent = showtime(m);
+    var s = "second"
+    second.textContent = showtime(s);
+}, 1000);  
